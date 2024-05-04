dce08a98f685907c49d8612927ec07985970559e
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-05-04 15:13:32 +0500

    Add files via upload

---
+++ GameLibrary/GameLibrary/GameObjects/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLibrary.GameObjects
+{
+    public class Game
+    {
+        public List<GameObject> gameObjectList;
+        public Form container;
+        private static Game Instance;
+        private static List<GameObject> EnemyObjects;
+        private static List<GameObject> PlayerObjects;
+        private static List<GameObject> Bullets;
+        private Game(Form container)
+        {
+            gameObjectList = new List<GameObject>();
+            EnemyObjects = new List<GameObject>();
+            PlayerObjects = new List<GameObject>();
+            Bullets = new List<GameObject>();
+            this.container = container;
+        }
+        public static Game GetValidInstance(Form Container)
+        {
+            if (Instance == null)
+            {
+                Instance = new Game(Container);
+            }
+            return Instance;
+        }
+        public void AddGameObject(Image img, int left, int top, IMovement controller, ObjectType Type)
+        {
+            GameObject gameObject = GameObject.GetObjectInstance(img, left, top, controller, Type);
+
+            if (Type == ObjectType.Enemy)
+            {
+                EnemyObjects.Add(gameObject);
+            }
+            else if (Type == ObjectType.Player)
+            {
+                PlayerObjects.Add(gameObject);
+            }
+            else if (Type == ObjectType.Bullet)
+            {
+                Bullets.Add(gameObject);
+            }
+            gameObjectList.Add(gameObject);
+            container.Controls.Add(gameObject.Pb);
+        }
+
+        public void Update()
+        {
+            foreach (GameObject go in gameObjectList)
+            {
+                go.update(go.GetPosition());
+                //CollissionDetector.PlayerVSEnemy(ObjectType.Player, ObjectType.Enemy);
+
+            }
+        }
+        public static List<GameObject> GetPlayers()
+        {
+            return PlayerObjects;
+        }
+        public static List<GameObject> GetEnemies()
+        {
+            return EnemyObjects;
+        }
+        public static List<GameObject> GetBullets()
+        {
+            return Bullets;
+        }
+
+    }
+}
---
+++ GameLibrary/GameLibrary/GameObjects/GameObject.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLibrary.GameObjects
+{
+    public class GameObject
+    {
+        public PictureBox Pb;
+        public IMovement controller;
+        private Point Position;
+        private static GameObject ObjectInstance;
+        private int Players = 0;
+        private int Enemies = 0;
+        private int Bullets = 0;
+        private ObjectType Type;
+        private int Score;
+        private float Health = 1000;
+        private GameObject(Image img, int left, int top, IMovement controller, ObjectType Type)
+        {
+            Position = new Point(left, top);
+            Pb = new PictureBox();
+            Pb.Image = img;
+            Pb.Location = Position;
+            if (Type == ObjectType.Player)
+                Pb.Size = new Size(img.Width, img.Height);
+            else if (Type == ObjectType.Enemy)
+                Pb.Size = new Size(img.Width / 2, img.Height / 2);
+            else if (Type == ObjectType.Bullet)
+                Pb.Size = new Size((img.Width / 2), (img.Height));
+            Pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            Pb.BackColor = Color.Transparent;
+            this.controller = controller;
+            SetObjectTypes(Type);
+        }
+        private GameObject() { }
+        protected GameObject GetGameObject() { return new GameObject(); }
+        public static GameObject GetObjectInstance(Image img, int left, int top, IMovement controller, ObjectType Type)
+        {
+            if ((FactoryPattern.GetMaxObstacles() <= 15) && (FactoryPattern.GetMaxPlayers() <= 5) && (FactoryPattern.GetMaxEnemies() <= 10) && (FactoryPattern.GetMaxBullets() <= 1000))
+            {
+                ObjectInstance = new GameObject(img, left, top, controller, Type);
+            }
+            else
+            {
+                throw new Exception("More than capacity production");
+            }
+            return ObjectInstance;
+        }
+
+        public int GetScore()
+        {
+            return Score;
+        }
+        public void SetScore(int Score)
+        {
+            this.Score = Score;
+        }
+        public void SetHealth(float Health)
+        {
+            this.Health = Health;
+        }
+        public float GetHealth()
+        {
+            return Health;
+        }
+        public void SetObjectTypes(ObjectType Type)
+        {
+            if (Type == ObjectType.Player)
+            {
+                FactoryPattern.SetMaxPlayers(Players += 1);
+
+            }
+            else if (Type == ObjectType.Enemy)
+            {
+                FactoryPattern.SetMaxEnemy(Enemies += 1);
+            }
+            else if (Type == ObjectType.Bullet)
+            {
+                FactoryPattern.SetMaxBullets(Bullets++);
+            }
+            this.Type = Type;
+        }
+        public ObjectType GetObjectType()
+        {
+            return Type;
+        }
+        public Point GetPosition()
+        {
+            return Position;
+        }
+        public void update(Point Location)
+        {
+            Position = controller.Move(Location);
+
+            Pb.Location = Position;
+        }
+    }
+}
